c2ae07359bcdb35319900f030eb61c68c2b59db7
lc1006 <1176168734@qq.com>
2025-06-16 18:18:41 +0800

    feat:update upm

---
+++ Standalone/Runtime/TapLicenseStandalone.cs
@@ -0,0 +1,42 @@
+﻿using TapSDK.License;
+using TapSDK.License.Internal;
+using TapSDK.Core;
+
+namespace TapSDK.License.Standalone {
+    public class TapLicenseStandalone : ITapLicenseBridge {
+        public void CheckLicense()
+        {
+            TapLogger.Warn($"{nameof(CheckLicense)} NOT implemented.");
+        }
+
+        public void CheckLicenseForcibly()
+        {
+            TapLogger.Warn($"{nameof(CheckLicenseForcibly)} NOT implemented.");
+        }
+
+        public void PurchaseDLC(string sku) {
+            TapLogger.Warn($"{nameof(PurchaseDLC)} NOT implemented.");
+        }
+
+        public void QueryDLC(string[] skus) {
+            TapLogger.Warn($"{nameof(QueryDLC)} NOT implemented.");
+        }
+
+        public void RegisterDLCCallback(ITapDlcCallback callback)
+        {
+            TapLogger.Warn($"{nameof(RegisterDLCCallback)} NOT implemented.");
+        }
+
+        public void RegisterLicencesCallback(ITapLicenseCallback callback)
+        {
+            TapLogger.Warn($"{nameof(RegisterLicencesCallback)} NOT implemented.");
+
+        }
+
+        public void SetTestEnvironment(bool isTest){
+             TapLogger.Warn($"{nameof(SetTestEnvironment)} NOT implemented.");
+        }
+
+       
+    }
+}
